3c9314271f3348f773493d08efe55c75e2caa4b9
qiaolu <ql0884@126.com>
2017-05-17 18:18:44 +0800

    11111111

---
+++ src/main/java/com/qiaolu/user/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.qiaolu.user.mapper;
+
+import java.util.List;
+
+import com.qiaolu.user.pojo.User;
+
+public interface UserMapper {
+	//这是查询所有的用户
+	public List<User> getUserList();
+
+}
---
+++ src/main/java/com/qiaolu/user/service/UserService.java
@@ -0,0 +1,7 @@
+package com.qiaolu.user.service;
+import java.util.List;
+import com.qiaolu.user.pojo.User;
+public interface UserService {
+	//这是查询所有用户
+	public List<User> getUserList();
+}
